48ec293cd21eadd70081d868705a32002868601a
havaed <havaed94@gmail.com>
2018-04-04 12:25:46 +0200

    Basic server

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const typeDefs =`
+type Query {
+    description: String
+}
+`
+const resolvers = {
+    Query: {
+        description: () => `This is the API for the project application`
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => console.log(`The server is running on http://localhost:4000`))
